a9f7b6722bbf0aac6a8b9304ad198f05e847a50a
Rochak Pandey <rochakpandey56@gmail.com>
2021-12-01 13:03:36 +0530

    fixed width of nav tabs

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,12 @@ navOpenBtn.addEventListener('click',(event)=>{
     nav.classList.add('nav-toggler');
 })
 
+window.onload=setWidth;
+window.onresize=setWidth;
+function setWidth(){
+    const buyNowWrapper=document.querySelector('.buy-now-wrapper')
+    const tabNavWrapper= document.querySelector('#tab-nav-wrapper')
+    let styles=window.getComputedStyle(buyNowWrapper);
+    let wide=styles.getPropertyValue('width');
+    tabNavWrapper.style.width=wide;
+}
